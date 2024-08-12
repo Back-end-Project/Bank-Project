91b6f22811c10a6e65b8098d31b527131820077a
Nicholas <razvan.botorog@yahoo.com>
2024-08-11 21:39:51 -0400

    updated welcoming message

--- src/main/java/com/demo/controller/Home.java
+++ src/main/java/com/demo/controller/Home.java
@@ -11,6 +11,6 @@ public class Home {
     @GetMapping("/home")
     ResponseEntity<?> getHome(){
         System.out.println("Welcome HOME");
-        return new ResponseEntity<>("You hit the endpoint!", HttpStatus.CREATED);
+        return new ResponseEntity<>("Welcome to our Bank APP! - This endpoint is for connectivity testing. Use Postman to enjoy the app - Swagger page is in work - Nicholas & Hector", HttpStatus.CREATED);
     }
 }
